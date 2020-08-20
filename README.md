3927f48e37c6e15129efe5eb9015a611b383f172
likangwei <likangwei@meituan.com>
2020-08-20 15:46:09 +0800

    init db

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # flywheel
+
+### install
+
+```
+>
+python3 -m venv venv
+source venv/bin/activate
+pip install django
+pip install djangorestframework
+python manage.py migrate
+```
--- website/flywheel/admin.py
+++ website/flywheel/admin.py
@@ -4,13 +4,19 @@ from .models import Flywheel
 from .models import WheelPart
 from .models import Situation
 
+
 class FlyWheelAdmin(admin.ModelAdmin):
     list_display = ['name']
 
     class Meta:
         fields = ['name']
 
+
+class SituationAdmin(admin.ModelAdmin):
+    list_display = ['situation', 'principle']
+
+
 admin.site.register(Flywheel, FlyWheelAdmin)
 admin.site.register(WheelPart)
-admin.site.register(Situation)
+admin.site.register(Situation, SituationAdmin)
 
